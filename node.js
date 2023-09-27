e0d3ef41487089f1ae6a263dc5412cf65d89e313
Sai Krishna <110606892+Saikrish77@users.noreply.github.com>
2023-09-27 21:15:23 +0530

    Create node.js

---
+++ node.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/signup", async function(req, res) {
+  const { signUpEmail, signUpPassword } = req.body; 
+
+  try {
+    // Check if the email is already in the database
+    const snapshot = await db.collection('mydata')
+      .where('Email', '==', signUpEmail)
+      .get();
+
+    if (!snapshot.empty) {
+      res.send("This email is already registered. Please <a href='/'>log in</a>.");
+    } else {
+      // If email is not present, proceed with registration
+      await db.collection('mydata').add({
+        Email: signUpEmail,
+        Password: signUpPassword
+      });
+      res.send("You Signed up Successfully with " + signUpEmail + ". <a href='/'>Click here to go to the login page</a>");
+    }
+  } catch (error) {
+    console.error("Error storing data:", error);
+    res.status(500).send("Error storing data.");
+  }
+});
+
+
+app.post('/login', async function(req, res) {
+  const { loginEmail, loginPassword } = req.body; 
+
+  try {
+    const snapshot = await db.collection('mydata')
+      .where('Email', '==', loginEmail)
+      .where('Password', '==', loginPassword)
+      .get();
+
+    if (!snapshot.empty) {
+      res.sendFile(__dirname + "/nutapi1.html");
+    } else {
+      res.send("Data not present in Firebase, please login");
+    }
+  } catch (error) {
+    console.error("Error querying data:", error);
+    res.status(500).send("Error querying data.");
+  }
+});
+
+const port = 200; 
+app.listen(port, function() {
+  console.log("Server started on port", port);
+});
